8ac3ddfa8b1d59cde3fa80665bee24d2214fb082
Brahim Chatri <brahim.chatri.dev@gmail.com>
2025-08-06 15:20:48 +0100

    remove data load and update settings.py

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -15,29 +15,6 @@ python manage.py migrate --noinput
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-echo "Creating superuser if not exists..."
-python manage.py shell << END
-from django.contrib.auth import get_user_model
-import os
-
-User = get_user_model()
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
-
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username=username, email=email or '', password=password)
-        print("Superuser created.")
-    else:
-        print("Superuser already exists.")
-else:
-    print("Superuser env vars not set; skipping creation.")
-END
-
-echo "starting load script to load data in db "
-python load_data.py
-
 echo "Starting Gunicorn..."
 exec gunicorn \
     --bind 0.0.0.0:8000 \
--- backend/job_board/settings.py
+++ backend/job_board/settings.py
@@ -60,7 +60,12 @@ SIMPLE_JWT = {
         "rest_framework_simplejwt.tokens.RefreshToken",
     ),
 }
+CSRF_TRUSTED_ORIGINS = ["https://api.chatri.dev"]
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Application definition
 
